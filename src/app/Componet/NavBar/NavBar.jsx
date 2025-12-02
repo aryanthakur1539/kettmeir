ce914b541597453ce347b96335b1dc1de4c3dbf5
Aryan-Thakur <aryanthakur1539@gmail.com>
2025-12-02 18:39:10 +0530

    add hamburger icon

--- src/app/Componet/NavBar/NavBar.jsx
+++ src/app/Componet/NavBar/NavBar.jsx
@@ -1,14 +1,28 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className={styles.navContainer}>
       <div className={styles.logoSection}>
         <img src="/logo.png" alt="Logo" width={60} height={60} />
       </div>
 
-      <ul className={styles.navLinks}>
+      {/* HAMBURGER ANIMATED ICON */}
+      <div
+        className={`${styles.hamburger} ${open ? styles.active : ""}`}
+        onClick={() => setOpen(!open)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* NAV LINKS */}
+      <ul className={`${styles.navLinks} ${open ? styles.openMenu : ""}`}>
         <li>MANIFESTO</li>
         <li>TERRITORIO</li>
         <li>IL METODO CLASSICO</li>
